bb8bdeb13f09119e790bd7444c961fedbea82019
Robert Spencer <robert.spencer94@gmail.com>
2017-09-02 11:12:49 +0200

    Removed dent
    [DO NOT BUILD FROM THIS COMMIT]

--- dent/Shaders/__init__.py
+++
@@ -1,5 +0,0 @@
-from core import *
-from GenericShader import GenericShader
-from InstancedShader import InstancedShader
-from TesselationShader import TesselationShader
-from TransformFeedbackShader import TransformFeedbackShader
